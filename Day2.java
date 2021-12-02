33652abeea8868367a25ebe614314e9aca79b8a2
CHWinkelman <charleshwinkelman@gmail.com>
2021-12-02 12:38:37 -0500

    Day 2 complete

--- Day2.java
+++ Day2.java
@@ -5,11 +5,11 @@ import java.io.FileNotFoundException;
 class Day2 {
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		/*------------part one----------------*/
-		System.out.print("Part one: ");
-
 		int x = 0;	//up or down
 		int y = 0;	//forward direction
+		int x2 = 0;     //part two variables
+		int y2 = 0;
+		int aim = 0;
 
 		File directions = new File("day2input.txt");
 		Scanner scanner = new Scanner(directions);
@@ -21,16 +21,21 @@ class Day2 {
 
 			switch (direction) {
 				case "forward": y += movement;
+						y2 += movement;
+						x2 += movement * aim;			
 						break;
 				case "up": x -= movement;
+					   aim -= movement;
 						break;
 				case "down": x += movement;
+					     aim += movement;
 						break;
 				default: System.out.println("Oops");
 						break;
 			}
 		}
 
-		System.out.println(x*y);
+		System.out.println("Part 1: " + (x*y));
+		System.out.println("Part 2: " + (x2*y2));
 	}
 }
